ce8c599c08091d63e174c859b71da98d64763d9b
Ryan "Brody" Wilcox <ryanbrodrick@gmail.com>
2022-12-31 15:02:44 -0800

    Refactor day 1 in Common Lisp

--- 2022/day1.lisp
+++ 2022/day1.lisp
@@ -1,27 +1,18 @@
 (in-package :aoc-2022)
 
-(defun day1-part1 (input-file)
+(defun day1-get-input-list (input-file)
   (with-open-file (input input-file)
-    (let ((input-list
-            (split-sequence:split-sequence
-             nil
-             (loop for line = (read-line input nil nil)
-                   while line
-                   if (string= line "")
-                     collect nil
-                   else
-                     collect (parse-integer line)))))
-      (reduce #'max (map 'list (lambda (l) (reduce #'+ l)) input-list)))))
+    (split-sequence:split-sequence
+     nil
+     (loop for line = (read-line input nil nil)
+           while line
+           if (string= line "")
+             collect nil
+           else
+             collect (parse-integer line)))))
+
+(defun day1-part1 (input-file)
+  (reduce #'max (map 'list (lambda (l) (reduce #'+ l)) (day1-get-input-list input-file))))
 
 (defun day1-part2 (input-file)
-  (with-open-file (input input-file)
-    (let ((input-list
-           (split-sequence:split-sequence
-            nil
-            (loop for line = (read-line input nil nil)
-               while line
-               if (string= line "")
-               collect nil
-               else
-               collect (parse-integer line)))))
-      (apply #'+ (subseq (sort (map 'list (lambda (l) (reduce #'+ l)) input-list) #'>) 0 3)))))
+  (apply #'+ (subseq (sort (map 'list (lambda (l) (reduce #'+ l)) (day1-get-input-list input-file)) #'>) 0 3)))
